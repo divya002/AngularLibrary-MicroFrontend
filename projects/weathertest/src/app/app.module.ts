00a48aa01ac184fdb1969766472f55885def28e1
DivyaPrakash <divyaprakash002@gmail.com>
2020-05-04 17:18:18 +0530

    angular library using tsconfig path to link

---
+++ projects/weathertest/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { WeatherModule } from 'weather';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    WeatherModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
